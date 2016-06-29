6bf17ff76eb04ee108019cc6c5b6f4296873f356
Anthony N. Simon <anthonynsimon@gmail.com>
2016-06-29 14:15:47 +0200

    added map smoothing

--- Assets/Code/MapGenerator.cs
+++ Assets/Code/MapGenerator.cs
@@ -8,6 +8,8 @@ public class MapGenerator : MonoBehaviour
     uint width;
     [SerializeField]
     uint height;
+	[SerializeField]
+	float depth;
 
     [SerializeField]
     Transform mapRoot;
@@ -15,6 +17,10 @@ public class MapGenerator : MonoBehaviour
     [Range(0f, 1f)]
     [SerializeField]
     float fillPercent;
+
+    [Range(0, 4)]
+    [SerializeField]
+    int smoothSteps;
 
     [SerializeField]
     int seed;
@@ -55,7 +61,11 @@ public class MapGenerator : MonoBehaviour
     {
         ConfigureRandomSeed();
         ClearMap();
-        map = BuildMapGrid();
+        BuildMapGrid();
+        for (int i = 0; i < smoothSteps; i++)
+        {
+            SmoothMap();
+        }
         GenerateGameObjects(map);
     }
 
@@ -64,9 +74,55 @@ public class MapGenerator : MonoBehaviour
         Random.seed = useRandomSeed ? Random.Range(int.MinValue, int.MaxValue) : seed;
     }
 
-    int[,] BuildMapGrid()
+    void SmoothMap()
     {
-        int[,] map = new int[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int wallCount = GetSurroundingWallCount(x, y);
+                if (wallCount > 4)
+                {
+                    map[x, y] = 1;
+                }
+                else if (wallCount < 4)
+                {
+                    map[x, y] = 0;
+                }
+            }
+        }
+    }
+
+    int GetSurroundingWallCount(int itemX, int itemY)
+    {
+        int wallCount = 0;
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                int currentX = itemX + x;
+                int currentY = itemY + y;
+
+                if (currentX == itemX && currentY == itemY)
+                {
+                    continue;
+                }
+                if (currentX < 0 || currentX >= width || currentY < 0 || currentY >= height)
+                {
+                    wallCount += 1;
+                }
+                else
+                {
+                    wallCount += map[currentX, currentY];
+                }
+            }
+        }
+        return wallCount;
+    }
+
+    void BuildMapGrid()
+    {
+        map = new int[width, height];
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -74,7 +130,6 @@ public class MapGenerator : MonoBehaviour
                 map[x, y] = (Random.Range(0, 1f) < fillPercent) ? 1 : 0;
             }
         }
-        return map;
     }
 
     void GenerateGameObjects(int[,] map)
@@ -84,8 +139,9 @@ public class MapGenerator : MonoBehaviour
             for (int y = 0; y < height; y++)
             {
                 GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                go.transform.position = new Vector3((-width / 2) + x + 0.5f, (-height / 2) + y + 0.5f, -map[x, y] / 2f);
-                go.transform.localScale = new Vector3(1, 1, map[x, y] + 1);
+				float zScale = (map[x, y] * depth) + 1;
+                go.transform.position = new Vector3((-width / 2) + x + 0.5f, (-height / 2) + y + 0.5f, -zScale / 2);
+                go.transform.localScale = new Vector3(1, 1, zScale);
                 if (mapRoot != null)
                 {
                     go.transform.SetParent(mapRoot);
